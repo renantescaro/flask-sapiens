dbb7597c79927923b28c6c9793a71f07c1e96b20
renantescaro <renantescaro@yahoo.com.br>
2021-10-20 14:36:09 -0300

    query de seleção de pedidos com produtos a serem faturados

---
+++ flaskr/dao/faturamento_dao.py
@@ -0,0 +1,33 @@
+import collections
+from flaskr.dao.banco import Banco
+
+class FaturamentoDao(Banco):
+    def pedido_com_produtos_faturar_nfe(self, id_ped_sapiens):
+        query = """ 
+                SELECT 
+                    ped.CODCLI, ped.NUMPED,
+                    item.CODPRO, item.CODDER,
+                    ped.CODFIL, item.SEQIPD,
+                    item.QTDVEN 
+                FROM SAPIENS.E120IPD item
+                INNER JOIN SAPIENS.E120PED ped
+                ON ped.NUMPED = item.NUMPED 
+                WHERE item.NUMPED = '"""+str(id_ped_sapiens)+"""'
+                AND item.CODREP = 999
+                AND item.CODEMP = 1
+                """
+
+        rows = self.executar(str(query))
+
+        objects_list = []
+        for row in rows:
+            d = collections.OrderedDict()
+            d['CODCLI'] = row[0]
+            d['NUMPED'] = row[1]
+            d['CODPRO'] = row[2]
+            d['CODDER'] = row[3]
+            d['CODFIL'] = row[4]
+            d['SEQIPD'] = row[5]
+            d['QTDVEN'] = row[6]
+            objects_list.append(d)
+        return objects_list
