640a383bc68c681335ac8b4f5643a6f6ca3fe787
renantescaro <renantescaro@yahoo.com.br>
2021-10-20 14:39:02 -0300

    erro

--- tests/webservices/sapiens/nfe_ws_test.py
+++ tests/webservices/sapiens/nfe_ws_test.py
@@ -1,21 +1,27 @@
 import unittest
 import sys
-sys.path.append('C:/Users/renan.tescaro/Desktop/flask/flask-sapiens')
+sys.path.append('C:/Users/renan.tescaro/Desktop/flask/flask-sapiens/')
 
 from flaskr.webservices.sapiens.nfe_ws import NfeSapiensWs
 from flaskr.entities.sapiens.nfe import NfeSapiens
+from flaskr.dao.faturamento_dao import FaturamentoDao
 
 class NfeSapiensWsTest:
     def __init__(self):
-        nfe_sapiens = NfeSapiens()
-        nfe_sapiens
-        nfe_sapiens
-        nfe_sapiens
-        nfe_sapiens
-        nfe_sapiens
-        nfe_sapiens
-        nfe_sapiens
-        retorno = NfeSapiensWs().gravar_notas_fiscais_saida(nfe_sapiens)
+
+        dados = FaturamentoDao().pedido_com_produtos_faturar_nfe(
+            id_ped_sapiens='3628' )
+
+        requisicao = NfeSapiens.parametros_requisicao(dados)
+        retorno    = NfeSapiensWs().gravar_notas_fiscais_saida(requisicao)
+
+        # print(requisicao)
         print(retorno)
+        if retorno['erroExecucao'] == None:
+            print('ok!')
+        else:
+            print('erro!')
+
+        print(retorno['mensagemRetorno'])
 
 NfeSapiensWsTest()
