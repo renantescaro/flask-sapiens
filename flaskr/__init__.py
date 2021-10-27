7ea0d97f00d5c7c406571b7b18eb1673462d00a0
renantescaro <renantescaro@yahoo.com.br>
2021-10-27 13:21:22 -0300

    adicionando registro de rotas

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, jsonify, url_for, send_file, redirect, session, request
-# from flaskr.routes.rotas import Rotas
+from flask import Flask
+from flaskr.controllers.faturamento_ctrl import bp as bp_faturamento
+
 
 def create_app(test_config=None):
     app = Flask(__name__, 
         static_url_path = '/static',
-        static_folder   = 'static',
-        instance_relative_config = True )
+        static_folder   = 'static' )
 
     app.config.from_mapping(
         SECRET_KEY   = 'super secret key',
         SESSION_TYPE = 'filesystem',
         JSONIFY_PRETTYPRINT_REGULAR = False )
-    # Rotas(app)
+
+    # adicionar rotas
+    app.register_blueprint(bp_faturamento)
     return app
