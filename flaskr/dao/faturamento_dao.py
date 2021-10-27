ee903537a955d914691a438f1b7283975972e8b8
renantescaro <renantescaro@yahoo.com.br>
2021-10-27 13:45:24 -0300

    ajustes indentação query

--- flaskr/dao/faturamento_dao.py
+++ flaskr/dao/faturamento_dao.py
@@ -2,25 +2,23 @@ import collections
 from flaskr.dao.banco import Banco
 
 class FaturamentoDao(Banco):
-    def pedido_com_produtos_faturar_nfe(self, id_ped_sapiens):
+    def pedido_com_produtos_faturar_nfe(self, id_ped_sapiens:int):
         query = """ 
-                SELECT 
-                    ped.CODCLI, ped.NUMPED,
-                    item.CODPRO, item.CODDER,
-                    ped.CODFIL, item.SEQIPD,
-                    item.QTDVEN 
-                FROM SAPIENS.E120IPD item
-                INNER JOIN SAPIENS.E120PED ped
-                ON ped.NUMPED = item.NUMPED 
-                WHERE item.NUMPED = '"""+str(id_ped_sapiens)+"""'
-                AND item.CODREP = 999
-                AND item.CODEMP = 1
-                """
-
-        rows = self.executar(str(query))
+            SELECT 
+                ped.CODCLI, ped.NUMPED,
+                item.CODPRO, item.CODDER,
+                ped.CODFIL, item.SEQIPD,
+                item.QTDVEN 
+            FROM SAPIENS.E120IPD item
+            INNER JOIN SAPIENS.E120PED ped
+            ON ped.NUMPED = item.NUMPED 
+            WHERE item.NUMPED = '"""+str(id_ped_sapiens)+"""'
+            AND item.CODREP = 999
+            AND item.CODEMP = 1
+            """
 
         objects_list = []
-        for row in rows:
+        for row in self.executar(str(query)):
             d = collections.OrderedDict()
             d['CODCLI'] = row[0]
             d['NUMPED'] = row[1]
