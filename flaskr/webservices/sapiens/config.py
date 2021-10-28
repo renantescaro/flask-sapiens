85e0514cf84a0f7fb3bcd1c29a99ca8b81607e10
renantescaro <renantescaro@yahoo.com.br>
2021-10-28 08:41:58 -0300

    uppercase no get da configuração

--- flaskr/webservices/sapiens/config.py
+++ flaskr/webservices/sapiens/config.py
@@ -2,4 +2,4 @@ from flaskr.utils.config import Config
 
 class ConfigSapiensWs:
     def __init__(self):
-        self.url = Config.get('sapiens_ws_url')
+        self.url = Config.get('SAPIENS_WS_URL')
