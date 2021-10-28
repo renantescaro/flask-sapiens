48269761365fc02150f6181ceefcf79da222ddff
renantescaro <renantescaro@yahoo.com.br>
2021-10-28 08:39:28 -0300

    adicionando instance_relative_config na config do flask

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,8 @@ from flaskr.controllers.faturamento_ctrl import bp as bp_faturamento
 def create_app(test_config=None):
     app = Flask(__name__, 
         static_url_path = '/static',
-        static_folder   = 'static' )
+        static_folder   = 'static',
+        instance_relative_config = True )
 
     app.config.from_mapping(
         SECRET_KEY   = 'super secret key',
