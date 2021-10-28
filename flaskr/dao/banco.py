2c6aa1c769f0c26da7b3486550c74ec2af128c30
renantescaro <renantescaro@yahoo.com.br>
2021-10-28 08:40:28 -0300

    adicionando tratamento de erro

--- flaskr/dao/banco.py
+++ flaskr/dao/banco.py
@@ -4,23 +4,32 @@ from flaskr.utils.config import Config
 
 class Banco:
     def __init__(self):
-        self._ip       = Config.get('banco_ip')
-        self._port     = Config.get('banco_porta')
-        self._bd       = Config.get('banco_nome')
-        self._username = Config.get('banco_usuario')
-        self._password = Config.get('banco_senha')
+        self._ip       = str(Config.get('BANCO_IP'))
+        self._port     = str(Config.get('BANCO_PORTA'))
+        self._username = str(Config.get('BANCO_USUARIO'))
+        self._password = str(Config.get('BANCO_SENHA'))
+        self._bd       = str(Config.get('BANCO_BANCO'))
         self._conn     = self._criar_conexao()
         self._cursor   = self._conn.cursor()
 
 
     def _criar_conexao(self):
-        dns = cx_Oracle.makedsn(self._ip, self._port, self._bd)
-        self._conn = cx_Oracle.connect(self._username, self._password, dns)
-        return self._conn
+        try:
+            dsn = cx_Oracle.makedsn(self._ip, self._port, self._bd)
+            return cx_Oracle.connect(
+                user     = self._username,
+                password = self._password,
+                dsn      = dsn )
+
+        except cx_Oracle.Error as err:
+            print(err)
+            return None
 
 
     def executar(self, query):
-        self._cursor.execute(query)
-        dados = self._cursor.fetchall()
-        self._conn.close()
-        return dados
+        if self._cursor is not None:
+            self._cursor.execute(query)
+            dados = self._cursor.fetchall()
+            self._conn.close()
+            return dados
+        return None
