97836fedb9ecf9f1f576b2418b9f653fc1e4c499
renantescaro <renantescaro@yahoo.com.br>
2021-10-28 08:38:56 -0300

    pegando configuração do env

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from waitress import serve
 from flaskr import create_app
+from flaskr.utils.config import Config
 
 app = create_app()
-serve(app, host='192.168.1.182', port=5001)
+serve(app, host=Config.get('IP_APLICACAO'), port=Config.get('PORTA_APLICACAO'))
